2f09e956c6775e29d3c91eaad1fe58460da0202c
SteppaSyde <S3754667@students.fscj.edu>
2026-09-24 20:14:23 -0400

    Update network_device.py

--- src/network_device.py/network_device.py
+++ src/network_device.py/network_device.py
@@ -1,14 +1,10 @@
 import logging
-
-
 class NetworkDevice:
     """Simple model of a network device."""
-
     def __init__(self, hostname, ip, type):
         self.hostname = hostname
         self.ip = ip
         self.type = type
-
     def summarize(self):
         """Print and log a one-line summary; also return it as a string."""
         msg = f"{self.hostname} ({self.type}) - {self.ip}"
